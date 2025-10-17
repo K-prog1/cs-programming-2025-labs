5eb8666b2b6d6d2238150e42492b8c673d20dfcc
Kupaki <gercovdaniil21@gmail.com>
2025-10-17 20:38:15 +1000

    Update labs4python.py

--- labs/labs 4/labs4python.py
+++ labs/labs 4/labs4python.py
@@ -41,8 +41,6 @@ def task_3():
     if sobak > 22:
         print("Возраст собаки не может превышать 22")
 
-task_3()
-
 def task_4():
     number = input("Введите любое число :")
     summa = sum(map(int,list(number)))
@@ -52,4 +50,40 @@ def task_4():
         print("Не делиться на 3")
 
 def task_5():
-    a = 0
+    password = input("ВВедите пароль: ")
+    spec_simvols = ".,:;!_*-+()/#¤%&"
+
+    has_upper = False
+    has_lower = False
+    has_number = False
+    has_spec_simvols = False
+    len_passsword = False
+
+    if len(password) >= 8:
+        len_passsword = True
+    else:
+        print("Код должен быть 8 строчным")
+
+    for i in password:
+        if i.isupper():
+            has_upper = True
+        if i.islower():
+            has_lower = True 
+        if i.isdigit():
+            has_number = True
+        if i in spec_simvols:
+            has_spec_simvols = True
+    if (len_passsword and has_lower and has_number and has_spec_simvols and has_upper) == True:
+        print("Пароль надежный! Он подходит")
+    
+    if not has_lower:
+        print("Нетус строчных")
+    if not has_number:
+        print("Нетуc цифр")
+    if not has_spec_simvols:
+        print("Нетус спец символов")
+    if not has_upper:
+        print("Нетус заглавных")
+
+task_5()
+            
