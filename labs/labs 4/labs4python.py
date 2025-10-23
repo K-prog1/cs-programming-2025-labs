79837cc36022f2eeb06072fdb0b2d87b92161f78
Kupaki <gercovdaniil21@gmail.com>
2025-10-23 20:41:38 +1000

    Update labs4python.py

--- labs/labs 4/labs4python.py
+++ labs/labs 4/labs4python.py
@@ -140,4 +140,16 @@ def task_9():
         print("Вечер")
     else:
         print("Введено больше 23 часов ")
-task_9()
+
+
+def task_10():
+    a = 0
+    chislo = int(input("Введите число: "))
+    for i in range(2, 9 + 1):
+        if chislo % i != 0:
+            a += 1
+    if a == 8:
+        print(f"{chislo} - простое число")
+    else:
+        print(f"{chislo} - составное число")
+task_10()
