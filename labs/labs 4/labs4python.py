28365e00cc8de6999049ab795f943b3770708b90
Kupaki <gercovdaniil21@gmail.com>
2025-10-23 20:30:28 +1000

    Update labs4python.py

--- labs/labs 4/labs4python.py
+++ labs/labs 4/labs4python.py
@@ -128,6 +128,16 @@ def task_8():
         print("Ваша скидка: 15%")
         print(sum - ((sum/100)*15))
     
-
-task_8()
-            
+def task_9():
+    sum = int(input(("Введите сумму товара: ")))
+    if sum <= 5:
+        print("Ночь")
+    elif 6 <= sum <= 11:
+        print("Утро")
+    elif 12 <= sum <= 17:
+        print("День")
+    elif 18<= sum <= 23:
+        print("Вечер")
+    else:
+        print("Введено больше 23 часов ")
+task_9()
