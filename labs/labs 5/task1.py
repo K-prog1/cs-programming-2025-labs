5b0bf6dda8a8ccebe563d95c3dc2eedaa9b77072
Пожиратель миров <gercovdaniil21@gmail.com>
2025-11-07 13:15:29 +1000

    labs5 - uptade

---
+++ labs/labs 5/task1.py
@@ -0,0 +1,36 @@
+def task_1():
+    spisok = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    for i in range(len(spisok)):
+        if spisok[i] == 3:
+            spisok[i] = 30
+    print(spisok)
+
+def task_2():
+    spisok = [1,2,3,4,5]
+    for i in range(len(spisok)):
+        spisok[i] = spisok[i]**2
+
+def task_3():
+    spisok = [23,55,12,23,7,8,100] 
+    print(max(spisok)/len(spisok))
+
+def task_4():
+    schet = 0
+    kortej = (1, "2", 3, "попа", 66, 43)
+    for i in kortej:
+        if type(i) is int:
+            schet += 1
+    if schet == len(kortej):
+        print('Все элементы числа')
+    else:
+        print("В кортежи не только числа")
+
+def task_5():
+    dicto = {"Молоко":1000, "Пепси-кола":1, "Мясо козла":9999}
+    highest_price = max(dicto, key = dicto.get)
+    lowest_price = min(dicto, key = dicto.get )
+    print("Самый дорогой товар:", highest_price,"-", dicto[highest_price])
+    print("Самый дешевый товар:", lowest_price,"-", dicto[lowest_price])
+
+task_5()
+
