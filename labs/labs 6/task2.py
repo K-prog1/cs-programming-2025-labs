18bf68966dc763a33cdc2bef37e36dbbd42bf2ef
Пожиратель миров <gercovdaniil21@gmail.com>
2025-12-19 12:00:20 +1000

    Push

--- labs/labs 6/task2.py
+++ labs/labs 6/task2.py
@@ -2,31 +2,39 @@ def bank_sistem():
     god_stavka = 0
     bonus_stavki = 0
     print("минимальный вклад 30000 рублей.")
-    vklad = input("Сделайте вклад указав срок в годах, на который собираетесь его класть: ").split(' ')
-    vkladint0 = int(vklad[0]) 
-    vkladint1 = int(vklad[1])
+    try:
+        vklad = input("Сделайте вклад указав срок в годах, на который собираетесь его класть: ").split(' ')
+        vkladint0 = int(vklad[0]) 
+        vkladint1 = int(vklad[1])
+    except:
+        print("Введите значения через пробел")
+
     if vkladint0 < 30000:
         print("Вклад не может быть меньше 30000 рублей")
-        return 
     else:
-        
-        for i in range(1, vkladint1):
-            if i <= 3:
-                bonus_stavki = 0.03
-            elif 4 <= i < 6:
-                bonus_stavki = 0.05
-            elif i >= 6:
-                bonus_stavki = 0.02
-
-        vkladint0 = vkladint0 + vkladint0 * bonus_stavki
+    
         c = vkladint0
-
-        while (c >= 10000) and (god_stavka < 5):
+        god_stavka = 0
+        while (c >= 10000) and (god_stavka < 0.05):
             c -= 10000
             god_stavka += 0.003
 
-        vkladint0 += vkladint0* bonus_stavki
-    print(vkladint0)
+        if vkladint1 <= 3:
+            bonus_stavki = 0.03
+        elif 4 <= vkladint1 <= 6:
+            bonus_stavki = 0.05
+        elif vkladint1 >= 6:
+            bonus_stavki = 0.02
+    
+    summa_stavok = god_stavka + bonus_stavki
+    vkladint2 = vkladint0
+
+    for god in range(vkladint1):
+        vkladint2 = vkladint2 *(1+summa_stavok)
+
+    vkladint3 = vkladint2 - vkladint0
+                        
+    print(vkladint3)
 
 bank_sistem()
 
---
+++ labs/labs 6/task3.py
@@ -0,0 +1,2 @@
+def Prostoe():
+    Enter = int(input("Введите 2 значеия через пробел"))
